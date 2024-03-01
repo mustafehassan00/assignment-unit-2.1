da16afae03b9cd6a5242c51fa8d10147e4f1c6de
Mustafe Hassan <mustafeh123@gmail.com>
2024-02-29 20:31:58 -0600

    Did the first, Last, and Full name line in about me.js

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,12 +1,13 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-let firstName = '';
+let firstName = 'Mustafe';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+let lastName = 'Hassan';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 //     (remember, you already have variables for this, can you use those?)
 //     Console log the value of `fullName`
-
+let fullName = 'Mustafe Hassan';
+console.log('fullName')
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 //     Console log the value of `luckyNumber`
 
