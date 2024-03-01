9dacb9ade2336b93f31ae6dc935eb840734add15
Mustafe Hassan <mustafeh123@gmail.com>
2024-02-29 20:41:27 -0600

    added a space in fullname variable so the first and last name are seperated

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -6,7 +6,7 @@ let lastName = 'Hassan';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 //     (remember, you already have variables for this, can you use those?)
 //     Console log the value of `fullName`
-let fullName = 'Mustafe Hassan';
+let fullName = firstName +' '+ lastName;
 console.log('fullName')
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 //     Console log the value of `luckyNumber`
