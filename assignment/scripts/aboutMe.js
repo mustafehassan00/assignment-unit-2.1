a7146994c6ae53668fe66b109d5a78c646c8118c
Mustafe Hassan <mustafeh123@gmail.com>
2024-03-03 00:39:14 -0600

    Did the introduction Variable and added a space in between my fullname variable

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -6,18 +6,19 @@ let lastName = 'Hassan';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 //     (remember, you already have variables for this, can you use those?)
 //     Console log the value of `fullName`
-let fullName = firstName  + lastName;
+let fullName = firstName  + ' ' + lastName;
 console.log('fullName');
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 //     Console log the value of `luckyNumber`
 let luckyNumber=3;
-console.log('luckyNumber');
+console.log(luckyNumber);
 // 5 - Create a variable `introduction` and using the variables from above,
 //     give it the value of:
 //     'My name is (full name), and I think (lucky number) is a winner!'.
 //     Refer back to the videos if you need help with this one.
 //     Console log the value of `introduction`
-
+let introduction='My name is ' + fullName + ' , and I think ' + luckyNumber + ' is a winner!';
+console.log(introduction);
 // 6 - Create a variable named `adventurous` and set it to a boolean value (true or false)
 //     Console log the value of `adventurous`
 
